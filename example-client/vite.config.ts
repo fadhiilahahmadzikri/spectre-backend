18c0658ee0c0bcab0ed4a87e604ded3994e5da3d
Zoex2304 <zikrifadhiilahahmad@gmail.com>
2026-05-13 01:44:36 +0700

    initial commit: on branch snap v4

--- example-client/vite.config.ts
+++ example-client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  esbuild: { jsx: "automatic", jsxImportSource: "react" },
+  plugins: [react(), tailwindcss()],
   server: { port: 3000 },
 });
